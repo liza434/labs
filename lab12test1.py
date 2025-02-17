c75662240b3a0aa1f8a00a12587c0a2a079efe69
liza434 <liza.fedotova.2020@list.ru>
2025-02-17 14:30:43 +0300

    Add files via upload

---
+++ lab12test1.py
@@ -0,0 +1,4 @@
+words = input().split()
+translations = input().split()
+dictionary = dict(zip(words, translations))
+print(dictionary)
---
+++ lab12test2.py
@@ -0,0 +1,10 @@
+text = input()
+words = text.split()
+word_count = {}
+for word in words:
+    word = word.lower()    
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+print(word_count)
